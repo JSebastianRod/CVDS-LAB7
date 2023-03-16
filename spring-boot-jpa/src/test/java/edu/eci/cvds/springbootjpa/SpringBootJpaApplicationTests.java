cf4dd422d5a9db73635d5a75b7a27e1efca8bf57
unknown <juan.rodriguez-pen@labinfo.is.escuelaing.edu.co>
2023-03-16 18:45:08 -0500

    Primer commit

---
+++ spring-boot-jpa/src/test/java/edu/eci/cvds/springbootjpa/SpringBootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds.springbootjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
