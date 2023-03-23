25d15063bf7e73a5d5389fa3a65215a374666e44
unknown <juan.rodriguez-pen@labinfo.is.escuelaing.edu.co>
2023-03-23 18:26:32 -0500

    Version 17 por que o si no corre spring

--- spring-boot-jpa/src/main/java/edu/eci/cvds/repository/EmployeeRepository.java
+++ spring-boot-jpa/src/main/java/edu/eci/cvds/repository/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package edu.eci.cvds.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import edu.eci.cvds.springbootjpa.Employee;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     
 }
